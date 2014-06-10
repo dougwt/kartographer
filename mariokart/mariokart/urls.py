dc8aa349b57843bbd5384d0512d3f8d7b95af029
Doug Tyler <dougwt@gmail.com>
2014-06-09 21:37:05 -0700

    Bugfix: Add missing urlconf for django debug
    See explicit setup: http://django-debug-toolbar.readthedocs.org/en/1.0/installation.html#explicit-setup

--- mariokart/mariokart/urls.py
+++ mariokart/mariokart/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 from django.contrib import admin
 admin.autodiscover()
@@ -10,3 +11,12 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# Uncomment the next line to serve media files in dev.
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += patterns('',
+                            url(r'^__debug__/', include(debug_toolbar.urls)),
+                            )
