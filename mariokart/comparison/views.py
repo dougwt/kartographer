7d0090d1aba047baf71a44bac993a0602045c125
Doug Tyler <dougwt@gmail.com>
2014-06-11 14:40:54 -0700

    Add initial template

--- mariokart/comparison/views.py
+++ mariokart/comparison/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
-    return HttpResponse("Hello, world. You're at the comparison home.")
+    # return HttpResponse("Hello, world. You're at the comparison home.")
+    context = {}
+    return render(request, 'home.html', context)
