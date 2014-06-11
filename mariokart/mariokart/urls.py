a4a240fc4e44af2455f1aecd11ba66433f8506d2
Doug Tyler <dougwt@gmail.com>
2014-06-11 13:43:00 -0700

    Add placeholder view to comparison

--- mariokart/mariokart/urls.py
+++ mariokart/mariokart/urls.py
@@ -6,7 +6,7 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'mariokart.views.home', name='home'),
+    url(r'^$', 'comparison.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
