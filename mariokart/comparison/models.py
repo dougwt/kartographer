fb15fb3f70c5c1c493b72fbe22200c9077e4a9d3
Doug Tyler <dougwt@gmail.com>
2014-06-12 10:19:47 -0700

    Split Racer model into Racer and RacerStats

--- mariokart/comparison/models.py
+++ mariokart/comparison/models.py
@@ -1,13 +1,20 @@
 from django.db import models
 
 # Create your models here.
-class Racer(models.Model):
+class RacerStats(models.Model):
     name = models.CharField(max_length=30)
     speed = models.DecimalField(max_digits=3, decimal_places=2)
     acceleration = models.DecimalField(max_digits=3, decimal_places=2)
     weight = models.DecimalField(max_digits=3, decimal_places=2)
     handling = models.DecimalField(max_digits=3, decimal_places=2)
     traction = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __unicode__(self):
+        return self.name
+
+class Racer(models.Model):
+    name = models.CharField(max_length=30)
+    stats = models.ForeignKey(RacerStats)
 
     def __unicode__(self):
         return self.name
