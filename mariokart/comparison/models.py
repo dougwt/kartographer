9154c00ce8445190fca7065ae7c9aeea401d222d
Doug Tyler <dougwt@gmail.com>
2014-06-11 20:21:31 -0700

    Add models

--- mariokart/comparison/models.py
+++ mariokart/comparison/models.py
@@ -1,3 +1,46 @@
 from django.db import models
 
 # Create your models here.
+class Racer(models.Model):
+    name = models.CharField(max_length=30)
+    speed = models.DecimalField(max_digits=3, decimal_places=2)
+    acceleration = models.DecimalField(max_digits=3, decimal_places=2)
+    weight = models.DecimalField(max_digits=3, decimal_places=2)
+    handling = models.DecimalField(max_digits=3, decimal_places=2)
+    traction = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __unicode__(self):
+        return self.name
+
+class Body(models.Model):
+    name = models.CharField(max_length=30)
+    speed = models.DecimalField(max_digits=3, decimal_places=2)
+    acceleration = models.DecimalField(max_digits=3, decimal_places=2)
+    weight = models.DecimalField(max_digits=3, decimal_places=2)
+    handling = models.DecimalField(max_digits=3, decimal_places=2)
+    traction = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __unicode__(self):
+        return self.name
+
+class Tire(models.Model):
+    name = models.CharField(max_length=30)
+    speed = models.DecimalField(max_digits=3, decimal_places=2)
+    acceleration = models.DecimalField(max_digits=3, decimal_places=2)
+    weight = models.DecimalField(max_digits=3, decimal_places=2)
+    handling = models.DecimalField(max_digits=3, decimal_places=2)
+    traction = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __unicode__(self):
+        return self.name
+
+class Glider(models.Model):
+    name = models.CharField(max_length=30)
+    speed = models.DecimalField(max_digits=3, decimal_places=2)
+    acceleration = models.DecimalField(max_digits=3, decimal_places=2)
+    weight = models.DecimalField(max_digits=3, decimal_places=2)
+    handling = models.DecimalField(max_digits=3, decimal_places=2)
+    traction = models.DecimalField(max_digits=3, decimal_places=2)
+
+    def __unicode__(self):
+        return self.name
