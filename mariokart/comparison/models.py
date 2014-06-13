a1370b2c6e8396535eacd69ba88def87ac59f6a6
Doug Tyler <dougwt@gmail.com>
2014-06-12 17:00:44 -0700

    Remove old static files & Update models

--- mariokart/comparison/models.py
+++ mariokart/comparison/models.py
@@ -1,53 +1,40 @@
 from django.db import models
 
 # Create your models here.
-class RacerStats(models.Model):
+class BaseModel(models.Model):
     name = models.CharField(max_length=30)
+
+    def __unicode__(self):
+        return self.name
+
+    def filename(self):
+        name = self.name.lower()
+        return name.translate(name.maketrans("",""), string.punctuation)
+
+    class Meta:
+        abstract = True
+
+class CommonStats(BaseModel):
     speed = models.DecimalField(max_digits=3, decimal_places=2)
     acceleration = models.DecimalField(max_digits=3, decimal_places=2)
     weight = models.DecimalField(max_digits=3, decimal_places=2)
     handling = models.DecimalField(max_digits=3, decimal_places=2)
     traction = models.DecimalField(max_digits=3, decimal_places=2)
 
-    def __unicode__(self):
-        return self.name
+    class Meta:
+        abstract = True
 
-class Racer(models.Model):
-    name = models.CharField(max_length=30)
+class RacerStats(CommonStats):
+    pass
+
+class Body(CommonStats):
+    pass
+
+class Tire(CommonStats):
+    pass
+
+class Glider(CommonStats):
+    pass
+
+class Racer(BaseModel):
     stats = models.ForeignKey(RacerStats)
-
-    def __unicode__(self):
-        return self.name
-
-class Body(models.Model):
-    name = models.CharField(max_length=30)
-    speed = models.DecimalField(max_digits=3, decimal_places=2)
-    acceleration = models.DecimalField(max_digits=3, decimal_places=2)
-    weight = models.DecimalField(max_digits=3, decimal_places=2)
-    handling = models.DecimalField(max_digits=3, decimal_places=2)
-    traction = models.DecimalField(max_digits=3, decimal_places=2)
-
-    def __unicode__(self):
-        return self.name
-
-class Tire(models.Model):
-    name = models.CharField(max_length=30)
-    speed = models.DecimalField(max_digits=3, decimal_places=2)
-    acceleration = models.DecimalField(max_digits=3, decimal_places=2)
-    weight = models.DecimalField(max_digits=3, decimal_places=2)
-    handling = models.DecimalField(max_digits=3, decimal_places=2)
-    traction = models.DecimalField(max_digits=3, decimal_places=2)
-
-    def __unicode__(self):
-        return self.name
-
-class Glider(models.Model):
-    name = models.CharField(max_length=30)
-    speed = models.DecimalField(max_digits=3, decimal_places=2)
-    acceleration = models.DecimalField(max_digits=3, decimal_places=2)
-    weight = models.DecimalField(max_digits=3, decimal_places=2)
-    handling = models.DecimalField(max_digits=3, decimal_places=2)
-    traction = models.DecimalField(max_digits=3, decimal_places=2)
-
-    def __unicode__(self):
-        return self.name
