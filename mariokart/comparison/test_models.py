736fbdf0f6bd287aa6672df8ccaebd2fde6b7c03
Doug Tyler <dougwt@gmail.com>
2014-06-26 17:04:31 -0700

    Add model tests

---
+++ mariokart/comparison/test_models.py
@@ -0,0 +1,60 @@
+"""Unit tests for comparison.models."""
+
+from django.http import HttpRequest
+from django.test import TestCase
+
+from .models import ConfigList, ConfigListItem, KartConfig
+
+
+class ConfigListTestCase(TestCase):
+    fixtures = [
+        'racer.yaml',
+        'racerstats.yaml',
+        'body.yaml',
+        'tire.yaml',
+        'glider.yaml',
+    ]
+
+    def test_create(self):
+        # Initialize mock request
+        mock_request = HttpRequest()
+        mock_request.META = {
+            'HTTP_X_REAL_IP': '127.0.0.1',
+            'REMOTE_ADDR': '172.31.233.133',
+        }
+
+        # Create a ConfigList
+        self.assertEqual(len(ConfigList.objects.all()), 0)
+        config_list = ConfigList.create(mock_request)
+        config_list.save()
+        self.assertEqual(len(ConfigList.objects.all()), 1)
+
+        # Create multiple ConfigListItems
+        self.assertEqual(len(ConfigListItem.objects.all()), 0)
+        test_kart_ids = (
+            (8, 19, 2, 2),  # Shyguy
+            (9, 2, 14, 2),  # Koopa Troopa
+        )
+        for kart in test_kart_ids:
+            config = KartConfig(kart)
+            item = ConfigListItem.create(
+                config_list,
+                config.racer,
+                config.body,
+                config.tire,
+                config.glider
+            )
+            item.save()
+        self.assertEqual(len(ConfigListItem.objects.all()), 2)
+
+        # Now try to load the ConfigList by url_hash
+        self.assertEqual(
+            ConfigList.objects.get(url=config_list.url),
+            config_list
+        )
+
+        # Ensure it contains the 2 ConfigListItems
+        result = ConfigListItem.objects.filter(list_id=config_list.id)
+        self.assertEqual(len(result), 2)
+        self.assertEqual(result[0].racer.id, 8)
+        self.assertEqual(result[1].racer.id, 9)
