a0766cd47983c79231b6b01a477db51dd6a8a6bb
Doug Tyler <dougwt@gmail.com>
2014-11-16 21:09:28 -0800

    Redesign 'Add Kart' template
    closes #20, closes #32

--- kartographer/comparison/urls.py
+++ kartographer/comparison/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
     url(r'^$', 'comparison.views.home', name='home'),
     url(r'^kart-components/', 'comparison.views.components', name='components'),
+    url(r'^add/', 'comparison.views.add', name='add'),
     url(r'^reset/', 'comparison.views.reset', name='reset'),
     url(r'^save/', 'comparison.views.save', name='save'),
     url(r'^l/(?P<url_hash>[0-9a-f]{5})/', 'comparison.views.list', name='list'),
